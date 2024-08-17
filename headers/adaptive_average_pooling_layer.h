7845aee6035778193d45eba324a2821caf5824b6
chart21 <chart21@web.de>
2024-08-18 00:33:36 +0200

    updated truncation approaches

--- headers/adaptive_average_pooling_layer.h
+++ headers/adaptive_average_pooling_layer.h
@@ -68,6 +68,8 @@ void AdaptiveAvgPool2d<T>::forward(const MatX<T>& prev_out, bool is_training)
 #endif
     T* out = this->output.data();
     const T* pout = prev_out.data();
+    auto denominators = new int[this->output.size()];
+    bool all_one = true;
 
     for (int n = 0; n < batch; ++n) {
         for (int c = 0; c < ch; ++c) {
@@ -87,57 +89,58 @@ void AdaptiveAvgPool2d<T>::forward(const MatX<T>& prev_out, bool is_training)
                             sum += pout[idx];
                         }
                     }
-
                     // Compute the average and assign it to the output
                     int out_idx = w + ow * (h + oh * (c + ch * n));
-                    out[out_idx] = sum.mult_public(w_end - w_start);
-#if TRUNC_APPROACH == 0 || TRUNC_APPROACH == 4
-                if (((h_end - h_start) & (h_end - h_start - 1)) == 0) // if power of 2
-                    out[out_idx] = out[out_idx].prepare_div_exp2(h_end - h_start);
-                else
-                    out[out_idx] *= FloatFixedConverter<float, INT_TYPE, UINT_TYPE, FRACTIONAL>::float_to_ufixed(1/(h_end - h_start));
-/* #elif TRUNC_THEN_MULT == 0 */
-#else
-                    out[out_idx] = out[out_idx].mult_public(FloatFixedConverter<float, INT_TYPE, UINT_TYPE, FRACTIONAL>::float_to_ufixed(1/(h_end - h_start)));
-#endif
+                    out[out_idx] = sum;
+                    int denominator = (h_end - h_start) * (w_end - w_start);
+                    if(denominator != 1)
+                        all_one = false;
+                    denominators[out_idx] = denominator;
                 }
             }
         }
     }
-    T::communicate();
+if(all_one)
+{
+print_online("All denominators in AdaptiveAvgPool2d are 1, skipping division");
+delete[] denominators;
+return;
+}
+
+for (int i = 0; i < this->output.size(); i++)
+{
 #if TRUNC_APPROACH == 0 || TRUNC_APPROACH == 4
-        for (int i = 0; i < this->output.size(); i++)
-            out[i].complete_public_mult_fixed();
+        if(denominators[i] <= 1)
+            continue;
+        if (((denominators[i]) & (denominators[i] - 1)) == 0) // if power of 2
+            out[i] = out[i].prepare_div_exp2(denominators[i]);
+        else
+            out[i] *= FloatFixedConverter<float, INT_TYPE, UINT_TYPE, FRACTIONAL>::float_to_ufixed(1/(denominators[i]));
+/* #elif TRUNC_THEN_MULT == 0 */
 #else
-//#if TRUNC_THEN_MULT == 1 */ //TODO: Check if this is needed
+            out[i] = out[i].mult_public(FloatFixedConverter<float, INT_TYPE, UINT_TYPE, FRACTIONAL>::float_to_ufixed(1/(denominators[i])));
+#endif
+}
+#if TRUNC_APPROACH == 0 || TRUNC_APPROACH == 4
+T::communicate();
+for (int i = 0; i < this->output.size(); i++)
+{
+        if(denominators[i] > 1)
+            out[i].complete_public_mult_fixed();
+}
+#endif
+
+delete[] denominators;
+
 #if TRUNC_APPROACH == 1 
-     trunc_2k_in_place(out, this->output.size(),false); // trunc before mult with demoniator */
+     trunc_2k_in_place(out, this->output.size(),true); // trunc before mult with demoniator */
  #elif TRUNC_APPROACH == 2 
      trunc_exact_in_place(out, this->output.size()); 
 #elif TRUNC_APPROACH == 3
-     trunc_exact_opt_in_place(out, this->output.size());
+     trunc_exact_opt_in_place(out, this->output.size(),true);
  #endif 
-#endif
-    /*     T::communicate(); */
 
 
-    /* for (int n = 0; n < batch; ++n) { */
-    /*     for (int c = 0; c < ch; ++c) { */
-    /*         for (int h = 0; h < oh; ++h) { */
-    /*             for (int w = 0; w < ow; ++w) { */
-    /*                 // Calculate the start and end indices for the kernel */
-    /*                 int h_start = h * stride_h; */
-    /*                 int h_end = std::min(h_start + kernel_h, ih); */
-    /*                 int w_start = w * stride_w; */
-    /*                 int w_end = std::min(w_start + kernel_w, iw); */
-    /*                 int out_idx = w + ow * (h + oh * (c + ch * n)); */
-    /*                 out[out_idx] = out[out_idx].mult_public(FloatFixedConverter<float, INT_TYPE, UINT_TYPE, FRACTIONAL>::float_to_ufixed(1/(h_end - h_start))); */
-    /*             } */
-    /*         } */
-    /*     } */
-    /* } */
-    /* #else */
-/* #endif */
 }
 
     template<typename T>
